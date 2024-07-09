172d11d7bcd52f0f7186c2355580f4f8c3e4e6b4
Say.vi <say.julia.vi@gmail.com>
2024-07-09 17:38:48 +0200

    mailing gonna be added with hosting

--- send-email.php
+++ send-email.php
@@ -4,3 +4,17 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 $phone = $_POST['phone'];
+
+require "PHPMailer-master/get_oauth_token.php"
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "sirrevia@gmail.com";
+$mail-> SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
